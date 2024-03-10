a0b0915c0c832a9408901924e79d25771875e7df
Yohan Lussiez <yohan.lussiez@valiantys.com>
2024-03-10 15:26:04 +0100

    Ajout Eureka

--- src/main/java/com/example/clients/ClientsApplication.java
+++ src/main/java/com/example/clients/ClientsApplication.java
@@ -2,8 +2,10 @@ package com.example.clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ClientsApplication {
 
     public static void main(String[] args) {
